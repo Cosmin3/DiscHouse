8593ba1c7594cb215265a36aa0365e678af0860e
Cosmin <cosmin_h6@yahoo.com>
2018-03-24 18:43:14 +0200

    Form Switching

--- DiscHouse/MainForm.Designer.cs
+++ DiscHouse/MainForm.Designer.cs
@@ -50,6 +50,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Proceed as Guest";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -94,6 +95,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Log In";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
--- DiscHouse/MainForm.cs
+++ DiscHouse/MainForm.cs
@@ -20,5 +20,31 @@ namespace DiscHouse
         {
             
         }
+
+        void closeForm(object sender, FormClosedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login newForm = new Login();
+            newForm.FormClosed += new FormClosedEventHandler(closeForm);
+            this.Hide();
+            newForm.Show();
+            newForm.Left = this.Left;
+            newForm.Top = this.Top;
+            
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Signup newForm = new Signup();
+            newForm.FormClosed += new FormClosedEventHandler(closeForm);
+            this.Hide();
+            newForm.Show();
+            newForm.Left = this.Left;
+            newForm.Top = this.Top;
+        }
     }
 }
